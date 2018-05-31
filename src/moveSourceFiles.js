d5109e4bc7fa376cfd8d7983f4b0bf7addf2ec38
Adam Stankiewicz <agstanki@gmail.com>
2018-05-31 17:03:29 -0400

    fix(dotfiles): include dot files in globby options

--- src/moveSourceFiles.js
+++ src/moveSourceFiles.js
@@ -6,7 +6,7 @@ import { SOURCE_FOLDER, SOURCE_FILENAME } from './constants';
 
 const moveSourceFiles = async ({ destination }) => {
   const destinationSourceFolder = path.join(destination, SOURCE_FOLDER);
-  const filePaths = await globby(destinationSourceFolder);
+  const filePaths = await globby(destinationSourceFolder, { dot: true });
   filePaths.forEach((filePath) => {
     const relativePath = filePath.split(SOURCE_FOLDER)[1];
     fse.moveSync(filePath, path.join(destination, relativePath));
